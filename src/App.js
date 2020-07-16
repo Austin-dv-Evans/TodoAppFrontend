53c1db593ec2c41cd6b6206efc5ebc301111af8c
Austin-Vincelli-Evans <unity7591@gmail.com>
2020-07-16 16:54:38 -0600

    build cards, style cards, todo container and item ccomponents work

--- src/App.js
+++ src/App.js
@@ -1,26 +1,32 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import './App.css';
+import TodoContainer from './Components/TodoContainer'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const todoURL = "http://localhost:3000/todos" 
+
+class App extends Component {
+  
+  state = {
+    todos: []
+  }
+
+  componentDidMount() {
+    this.getTodos()
+  }
+
+  getTodos = () => {
+    fetch(todoURL)
+      .then(response => response.json())
+      .then(todos => this.setState({todos}))
+  }
+  render(){
+    return (
+      <div className="App">
+        <h1>Todo App</h1>
+        <TodoContainer todos={this.state.todos}/>
+      </div>
+    )
+  }
 }
 
 export default App;
---
+++ src/Components/TodoItem.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+
+export default function TodoItem({title, content}) {
+
+
+
+
+    return(
+        <li className="todo-item">
+            <h2>{title}</h2>
+            <h3>{content}</h3>
+        </li>
+    )
+}
