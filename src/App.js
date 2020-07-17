8e59c942e68906a89204a1791f26c4161a2a01df
Austin-Vincelli-Evans <unity7591@gmail.com>
2020-07-17 17:29:26 -0600

    TodoForm persiting to backend, delete card persiting, styless updated

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, {Component} from 'react';
 import './App.css';
 import TodoContainer from './Components/TodoContainer'
+import TodoForm from './Components/TodoForm'
 
 const todoURL = "http://localhost:3000/todos" 
 
@@ -19,11 +20,36 @@ class App extends Component {
       .then(response => response.json())
       .then(todos => this.setState({todos}))
   }
+
+  addTodo = (newTodo) => {
+    this.setState({
+      todos: [...this.state.todos, newTodo]
+    })
+
+    fetch(todoURL, {
+      method: "POST",
+      headers: {
+        "Content-Type" : "application/json"
+      },
+      body: JSON.stringify(newTodo)
+    })
+  }
+
+  deleteTodo = (id) => {
+    let filtered =  this.state.todos.filter(todo => todo.id !== id)
+    this.setState({
+      todos: filtered
+    })
+    fetch(todoURL + "/" + id, { method: "DELETE" })
+  }
+
   render(){
     return (
       <div className="App">
         <h1>Todo App</h1>
-        <TodoContainer todos={this.state.todos}/>
+        <TodoForm addTodo={this.addTodo}/>
+        <TodoContainer deleteTodo={this.deleteTodo} todos={this.state.todos}/>
+        
       </div>
     )
   }
