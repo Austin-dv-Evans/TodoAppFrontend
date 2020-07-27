8cb9d62a104c274a12010235a0e699241fcbdd36
Austin-Vincelli-Evans <unity7591@gmail.com>
2020-07-27 16:46:39 -0600

    add sign up form, add alert and user state

--- src/App.js
+++ src/App.js
@@ -2,14 +2,17 @@ import React, {Component} from 'react';
 import './App.css';
 import TodoContainer from './Components/TodoContainer'
 import TodoForm from './Components/TodoForm'
-
+import SignUpForm from './Components/SignUpForm';
 const todoURL = "http://localhost:3000/todos/" 
 
 class App extends Component {
   
   state = {
-    todos: []
+    todos: [],
+    user: {},
+    alerts: []
   }
+
 
   componentDidMount() {
     this.getTodos()
@@ -57,10 +60,34 @@ class App extends Component {
     fetch(todoURL +  id, { method: "DELETE" })
   }
 
+  signUp = (user) => {
+    fetch("http://localhost:3000/users", {
+      method: "POST",
+      headers: {
+          "Content-Type": "application/json"
+        },
+      body: JSON.stringify({user})
+    })
+    .then(response => response.json())
+    .then(response => {
+      if(response.errors){
+        this.setState({alerts: response.errors})
+      }
+      else {
+        localStorage.setItem('token', response.token)
+        this.setState({
+          user: response.user,
+          alerts: ["User successfully created!"]
+        })
+      }
+    })
+  }
+
   render(){
     return (
       <div className="App">
         <h1>Todo App</h1>
+        <SignUpForm signUp={this.signUp} alerts={this.state.alerts}/>
         <TodoForm submitAction={this.addTodo}/>
         <TodoContainer updateTodo={this.updateTodo} deleteTodo={this.deleteTodo} todos={this.state.todos}/>
         
